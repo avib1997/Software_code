4e238cc75deba47579711313785ddd8dbe53f884
‪Avi Brodetsky‬‏ <95278057+avib1997@users.noreply.github.com>
2023-09-14 10:22:53 +0300

    Add files via upload

---
+++ vsc_python/basic.py
@@ -0,0 +1,11 @@
+import datetime
+
+def avg(myList):
+    return sum(myList)/len(myList)
+
+# print(datetime.datetime.now())
+# print(avg([1,2,3]))
+mas = input("your name: ")
+print("hello %s!" % mas)
+print(f"hello {mas}!")
+
